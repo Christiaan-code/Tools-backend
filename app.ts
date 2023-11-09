0e3f18e53dc836f5ec322190957f4046755abb24
Christiaan du Preez <christiaan.d@jobjack.co.za>
2023-11-09 09:09:30 +0200

    Remove comments

--- app.ts
+++ app.ts
@@ -9,46 +9,35 @@ const port = 1000
 
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send('Hello, Express!')
-})
+app.get('/', (req, res) => {})
 
-// Middleware to parse JSON request body
 app.use(bodyParser.json());
 
-// Define a route to handle POST requests
 app.post('/decrypt', (req, res) => {
-  // Access the JSON data from the request body
   const requestData: string = req.body.data
   console.log(requestData)
 
   const decryptedValue = CryptoHandler.decrypt(requestData)
 
   res.json({ message: 'Received POST data', data: decryptedValue.toString() });
-
 });
 
 app.post('/encrypt', (req, res) => {
-  // Access the JSON data from the request body
   const requestData: string = req.body.data
   console.log(requestData)
 
   const decryptedValue = CryptoHandler.encrypt(requestData)
 
   res.json({ message: 'Received POST data', data: decryptedValue.toString() });
-
 });
 
 app.post('/one-way-encrypt', (req, res) => {
-  // Access the JSON data from the request body
   const requestData: string = req.body.data
   console.log(requestData)
 
   CryptoHandler.OneWayEncrypt(requestData).then((val) => {
     res.json({ message: 'Received POST data', data: val.toString() });
   })
-
-
 });
 
 
